dca4c069bf8862b253b07580d8547fecc3f6c166
tiramisu24 <diane.z.zheng@gmail.com>
2017-04-04 11:20:56 -0700

    move intent for testing purpose

---
+++ app/src/AREALITY/app/src/main/java/com/example/dianezheng/areality/LandmarkPage.java
@@ -0,0 +1,13 @@
+package com.example.dianezheng.areality;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LandmarkPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landmark_page);
+    }
+}
