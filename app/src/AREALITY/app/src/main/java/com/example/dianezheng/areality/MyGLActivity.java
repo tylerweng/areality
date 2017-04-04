7d52868bf17f416f315ee1813174ab9a4228fbfc
tiramisu24 <diane.z.zheng@gmail.com>
2017-04-04 11:30:40 -0700

    add MyGLActivity

---
+++ app/src/AREALITY/app/src/main/java/com/example/dianezheng/areality/MyGLActivity.java
@@ -0,0 +1,33 @@
+package com.example.dianezheng.areality;
+
+import android.opengl.GLSurfaceView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyGLActivity extends AppCompatActivity {
+
+
+    private GLSurfaceView glView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_gl);
+
+        glView = new GLSurfaceView(this);           // Allocate a GLSurfaceView
+        glView.setRenderer(new MyGLRenderer(this)); // Use a custom renderer
+        this.setContentView(glView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        glView.onPause();
+    }
+
+    // Call back after onPause()
+    @Override
+    protected void onResume() {
+        super.onResume();
+        glView.onResume();
+    }
+}
