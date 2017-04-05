2b9fb01693afa1ebc9a8b8952cc9dc2b1e08db57
tiramisu24 <diane.z.zheng@gmail.com>
2017-04-04 17:12:50 -0700

    move logic into landmark page

--- app/src/AREALITY/app/src/main/java/com/example/dianezheng/areality/LandmarkPage.java
+++ app/src/AREALITY/app/src/main/java/com/example/dianezheng/areality/LandmarkPage.java
@@ -1,13 +1,32 @@
 package com.example.dianezheng.areality;
 
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LandmarkPage extends AppCompatActivity {
+public class LandmarkPage extends Activity {
 
-    @Override
+    private GLSurfaceView glView;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_landmark_page);
+        setContentView(R.layout.activity_my_gl);
+
+        glView = new GLSurfaceView(this);           // Allocate a GLSurfaceView
+        glView.setRenderer(new MyGLRenderer(this)); // Use a custom renderer
+        this.setContentView(glView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        glView.onPause();
+    }
+
+    // Call back after onPause()
+    @Override
+    protected void onResume() {
+        super.onResume();
+        glView.onResume();
     }
 }
