341679a755052e56749d41346a3f2867a46b5301
tiramisu24 <diane.z.zheng@gmail.com>
2017-04-07 10:51:49 -0700

    cube slows down

--- app/src/AREALITY/app/src/main/java/com/example/areality/MyGLRenderer.java
+++ app/src/AREALITY/app/src/main/java/com/example/areality/MyGLRenderer.java
@@ -68,7 +68,15 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
         gl.glTranslatef(0.0f, 0.0f, -6.0f);   // Translate into the screen
         gl.glRotatef(angleCube, xRot, yRot, 0.3f); // Rotate
         cube.draw(gl);
-
+        if(speedCube > 0.5f){
+            speedCube -= 1f;
+        }else if (speedCube > 30f){
+            speedCube = 30f;
+        }else if (speedCube < 0){
+            speedCube *= -1;
+        }else{
+            speedCube = 0.5f;
+        }
         // Update the rotational angle after each refresh.
         angleCube += speedCube;
     }
@@ -91,8 +99,7 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
 
         xRot = x;
         yRot = y;
+        setSpeedCube((x+y)*10);
     }
 }
-
-
 
