55a713429ba0ae1f2f97953f0970b2ae2c590044
Tyler Weng <tylercweng@gmail.com>
2017-04-15 22:39:10 -0700

    update my places functionality (almost working)

--- app/src/AREALITY/app/src/main/java/areality/game/MyPlacesActivity.java
+++ app/src/AREALITY/app/src/main/java/areality/game/MyPlacesActivity.java
@@ -9,6 +9,7 @@ import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
@@ -27,8 +28,18 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,6 +49,7 @@ import butterknife.OnClick;
 public class MyPlacesActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private static final String LANDMARK_ID = "Landmark Marker";
     private static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
     private List<String> landmarkIds = new ArrayList<>();
     GoogleApiClient mGoogleApiClient;
@@ -65,9 +77,6 @@ public class MyPlacesActivity extends FragmentActivity implements OnMapReadyCall
                 .findFragmentById(R.id.map2);
         mapFragment.getMapAsync(this);
 
-        retrieveLandmarkIds();
-
-        Toast.makeText(getBaseContext(), landmarkIds.get(0), Toast.LENGTH_LONG).show();
     }
 
     @OnClick(R.id.profileButton) void switchToProfile() {
@@ -78,6 +87,7 @@ public class MyPlacesActivity extends FragmentActivity implements OnMapReadyCall
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        retrieveLandmarkIds();
         setMarkers();
     }
 
@@ -137,11 +147,81 @@ public class MyPlacesActivity extends FragmentActivity implements OnMapReadyCall
     }
 
     private void setMarkers() {
-        // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
+        Toast.makeText(getBaseContext(), landmarkIds.get(0), Toast.LENGTH_LONG).show();
+
+        for (int i = 0; i < landmarkIds.size(); i++) {
+            String landmarkId = landmarkIds.get(i);
+            String detailUrl = getDetailUrl(landmarkId);
+            String result = "";
+            String name = "";
+            JSONObject jsonObject = null;
+            LatLng latLng = null;
+            try {
+                result = makeHTTPRequest(detailUrl);
+                jsonObject = new JSONObject(result);
+                name = jsonObject.getJSONObject("result").getString("name");
+                latLng = getLatLng(jsonObject);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            mMap.addMarker(new MarkerOptions()
+                    .position(latLng)
+                    .title(name)
+                    .snippet(landmarkId));
+
+            if (i == landmarkIds.size() - 1) mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        }
+
+        mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+                Intent intent = new Intent(getApplicationContext(), LandmarkPage.class);
+                String landmarkId = marker.getSnippet();
+                intent.putExtra(LANDMARK_ID, landmarkId);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private LatLng getLatLng(JSONObject jsonObject) throws JSONException {
+        JSONObject location = jsonObject.getJSONObject("result").getJSONObject("geometry").getJSONObject("location");
+        Double lat = location.getDouble("lat");
+        Double lng = location.getDouble("lng");
+        return new LatLng(lat, lng);
+    }
+
+    private String getDetailUrl(String placeId) {
+
+        StringBuilder googlePlacesDetailUrl = new StringBuilder("https://maps.googleapis.com/maps/api/place/details/json?");
+        googlePlacesDetailUrl.append("placeid=" + placeId);
+        googlePlacesDetailUrl.append("&key=" + "AIzaSyB3IPyQhFjPS0kysn8Fh9xxhZ2SN12ek1Y");
+        return (googlePlacesDetailUrl.toString());
+    }
+
+    protected String makeHTTPRequest(String urlString) throws IOException {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        HttpURLConnection connection;
+        try {
+            URL url = new URL(urlString);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setDoOutput(true);
+            connection.connect();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String content = "", line;
+            while ((line = rd.readLine()) != null) {
+                content += line + "\n";
+            }
+            return content;
+        } catch (IOException e) {
+            Log.d("error", e.toString());
+            return e.toString();
+        }
     }
 
 }
